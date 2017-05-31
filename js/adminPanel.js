062f03d042c7d4f7f6ba08e69e46caa508b0f517
MIchael LeGrand <michael_legrand@yahoo.com>
2017-05-30 22:50:13 -0400

    Minor polish. Making resize happen. Making clicks feel nice. Rounding image. Removing popups.

--- js/adminPanel.js
+++ js/adminPanel.js
@@ -1,5 +1,9 @@
 var _token;//auth token passed from server through url.
 
+var _selecedRegions = [
+  false, false, false, false, false, false, false, false, false
+]
+
 $( document ).ready(function() {
   var params = getQueryParams(location.search);
   //console.log(JSON.stringify(params));
@@ -8,7 +12,10 @@ $( document ).ready(function() {
     window.location.href = "http://mike-legrand.com/bad_batch_alert_web_admin/index.html"
   }
 
-  
+  $('path').mouseover(onPathMouseover);
+  $('path').mouseleave(onPathMouseleave);
+  $('path').click(onPathClick);
+
 });
 
 function getQueryParams(qs) {
@@ -23,4 +30,40 @@ function getQueryParams(qs) {
   }
 
   return params;
+}
+
+function onPathMouseover()
+{
+  $('.mapImage').addClass('highlight');
+}
+
+function onPathMouseleave()
+{
+  $('.mapImage').removeClass('highlight');
+}
+
+function onPathClick()
+{
+
+  var index = undefined;
+  var paths = $('path');
+  for (var i = 0; i < paths.length; i++) {
+    if (paths[i] == this) {
+      index = i; 
+      break;
+    }
+  }
+  var mapMarker = $('.map-marker').eq(index);
+
+  var classes = this.getAttribute('class');
+  turnOff = (classes.indexOf('selected') !== -1);
+  if (turnOff) {
+    classes = classes.replace('selected', ''); 
+    mapMarker.hide();
+  } else {
+    classes = classes + ' selected';
+    mapMarker.show();
+  }
+
+  $(this).attr("class", classes);
 }
